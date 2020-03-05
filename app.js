c4a0bba1bcea2c7d41495499a32a1cf1e9bb69b1
phamthai12316 <phamthai12316@gmail.com>
2020-03-05 11:01:13 +0700

    MailChimpDemo

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-const http = require("http");
-const port = process.env.PORT || 3000;
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var querystring = require("querystring");
+var mailchimpClientId = "xxxxxxxxxxxxxxxx";
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html");
-  res.end("<h1>Hello World</h1>");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req, res) {
+  res.send("Hello World!");
 });
 
-server.listen(port, () => {
-  console.log(`Server running at port ` + port);
+app.get("/mailchimp/auth/authorize", function(req, res) {
+  res.redirect(
+    "https://login.mailchimp.com/oauth2/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: mailchimpClientId,
+        redirect_uri: "http://127.0.0.1:3000/mailchimp/auth/callback"
+      })
+  );
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
